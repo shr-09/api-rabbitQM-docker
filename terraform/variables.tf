ebd95c3040d410b13936f1c303456f4d814ebfbd
Santiago <henao0301509@gmail.com>
2026-04-12 14:04:54 -0500

    Mover variables de terraform a tfvars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,22 +1,14 @@
 # Definimos las variables para no hardcodear todo si es posible
-variable "vpc_id" {
-  default = "vpc-0db51e24b1be9bc68"
-}
+variable "vpc_id" {}
 
-variable "subnet_id" {
-  default = "subnet-0a279d9c60a8ef4e0"
-}
+variable "subnet_id" {}
 
-variable "subnet_id_2" {
-  default = "subnet-0030ee3b0b7300432"
-}
+variable "subnet_id_2" {}
+
+variable "key_name" {}
 
 variable "ami_id" {
   default = "ami-02dfbd4ff395f2a1b" # Amazon Linux 2023
-}
-
-variable "key_name" {
-  default = "shr"
 }
 
 variable "instance_type" {
